52154c8b9dd6b6633b49ba16519a732dbc14ccd3
ericacrowe24 <ericacrowe24@gmail.com>
2021-08-17 01:39:40 -0400

    Added weather and evolution

---
+++ ProfMon.Framework/Monster/IEvolution.cs
@@ -0,0 +1,28 @@
+﻿using ProfMon.Framework.Item;
+using ProfMon.Framework.World;
+
+namespace ProfMon.Framework.Monster {
+    public interface IEvolution {
+        ISpecies Target { get; }
+
+        int LevelRequired { get; }
+
+        IItem ItemRequired { get; }
+
+        IWeather WeatherRequired { get; }
+
+        ISpecies TeamMateRequired { get; }
+
+        public interface IEvolutionBuilder : IBuilder<IEvolution, IEvolutionBuilder> {
+            IEvolutionBuilder WithTarget (ISpecies species);
+
+            IEvolutionBuilder WithLevelRequired (int level);
+
+            IEvolutionBuilder WithItemRequired (IItem item);
+
+            IEvolutionBuilder WithWeatherRequired (IWeather weather);
+
+            IEvolutionBuilder WithTeamMateRequired (ISpecies tm);
+        }
+    }
+}
---
+++ ProfMon.Framework/World/IWeather.cs
@@ -0,0 +1,11 @@
+﻿using ProfMon.Framework.Descriptors;
+using ProfMon.Framework.Monster;
+using System.Collections.Generic;
+
+namespace ProfMon.Framework.World {
+    public interface IWeather : IName, IDescription {
+        int Duration { get; }
+
+        Dictionary<IElement, float> ElementModifiers { get; }
+    }
+}
