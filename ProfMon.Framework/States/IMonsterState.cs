fabb0257033436815e05352d6a88bb27820625e4
ericacrowe24 <ericacrowe24@gmail.com>
2021-08-21 22:25:59 -0400

    Added two stats: Monster and Move

---
+++ ProfMon.Framework/States/IMonsterState.cs
@@ -0,0 +1,52 @@
+﻿using ProfMon.Framework.Item;
+using ProfMon.Framework.Monster;
+
+namespace ProfMon.Framework.States {
+    public interface IMonsterState {
+        public ISpecies Species { get; }
+
+        public int Level { get; }
+        public float Experience { get; }
+        public bool CanLevelUp { get; }
+
+        public ITrait[] Traits { get; }
+
+        public string Nickname { get; }
+        public bool CanBeRenamed { get; }
+
+        public float MaxHealth { get; }
+        public float CurrentHealth { get; }
+
+        public float PhysicalAttack { get; }
+        public float PhyiscalDefense { get; }
+        public float NonphysicalAttack { get; }
+        public float NonphysicalDefense { get; }
+        public float Speed { get; }
+
+        public IStatus[] Statuses { get; }
+
+        public IItem HeldItem { get; }
+
+        public IMoveState[] Moves { get; }
+
+        void Rename (string newName);
+
+        void GiveItem (IItem item);
+
+        IItem RemoveItem ();
+
+        void Heal (float amount);
+
+        void Damage (float amount);
+
+        void GiveStatus (IStatus status);
+
+        void RemoveStatus ();
+
+        void UseMove (int slot, int amount);
+
+        void RestoreMove (int slot, int amount);
+
+        void Evolve (ISpecies target);
+    }
+}
---
+++ ProfMon.Framework/States/IMoveState.cs
@@ -0,0 +1,23 @@
+﻿using ProfMon.Framework.Monster.Move;
+
+namespace ProfMon.Framework.States {
+    public interface IMoveState {
+        IMove Move { get; }
+
+        int Slot { get; }
+
+        int CurrentUses { get; }
+
+        int MaxUses { get; }
+
+        int BoostedAmount { get; }
+
+        bool Boostable { get; }
+
+        void Boost (int amount);
+
+        void Use (int uses = 1);
+
+        void Restore (int amt = 1);
+    }
+}
